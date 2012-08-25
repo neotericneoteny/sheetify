000c21d75e1bac668744031722573e67b3b8a39e
Matthew Zimmer <neoteric.neoteny@gmail.com>
2012-08-24 20:00:10 -0400

    Implemented options.hideAfterMillis. This option allows one to have the sheet automatically removed after 'n' milliseconds.

--- jquery.sheetify.js
+++ jquery.sheetify.js
@@ -11,111 +11,121 @@
  * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
  */
 (function(window) {
-	'use strict';
+    'use strict';
 
     var $ = window.jQuery;
 
-	// CSS Class Names
-	var SHEETIFY_CSS_CONTAINER_CLASS = 'neo-overlay-sheet';
-	var SHEETIFY_CSS_PROMPT_CLASS = 'prompt';
+    // CSS Class Names
+    var SHEETIFY_CSS_CONTAINER_CLASS = 'neo-overlay-sheet';
+    var SHEETIFY_CSS_PROMPT_CLASS = 'prompt';
 
-	// Commands
-	var REMOVE_COMMAND = 'remove';
-	var COVER_COMMAND = 'cover';
+    // Commands
+    var REMOVE_COMMAND = 'remove';
+    var COVER_COMMAND = 'cover';
 
-	$.fn.sheetify = function( options ) {
+    $.fn.sheetify = function( options ) {
 
-		options = $.extend({
-			command : COVER_COMMAND,
-			color : 'rgb(255,255,255)',
-			opacity : 0.75,
-			altClassName: undefined,
-			message: undefined
-		}, options );
+        options = $.extend({
+            command : COVER_COMMAND,
+            color : 'rgb(255,255,255)',
+            opacity : 0.75,
+            altClassName: undefined,
+            message: undefined,
+			hideAfterMillis:0
+        }, options );
 
-		switch( options.command ) {
+		var target = this;
 
-			case REMOVE_COMMAND:
-				$( this ).children( '.'+SHEETIFY_CSS_CONTAINER_CLASS ).remove();
-				break;
-			default:
-				// Remove any sheets from the current element
-				$(this ).sheetify({command:REMOVE_COMMAND});
+        switch( options.command ) {
 
-				var border = {
-					t: parseNumberOrDefaultValue( this.css( 'border-top' ), 0 ),
-					b: parseNumberOrDefaultValue( this.css( 'border-bottom' ), 0 ),
-					l: parseNumberOrDefaultValue( this.css( 'border-left' ), 0 ),
-					r: parseNumberOrDefaultValue( this.css( 'border-right' ), 0 )
-				};
-				var h = parseNumberOrDefaultValue( this.innerHeight(true), 0 ),
-					w = parseNumberOrDefaultValue( this.innerWidth(false), 0 ),
-					l = parseNumberOrDefaultValue( this.offset().left + border.l, 0 ),
-					t = parseNumberOrDefaultValue( this.offset().top + border.t, 0 ),
-					r = parseNumberOrDefaultValue( l+w+border.r, 0 ),
-					b = parseNumberOrDefaultValue( t+h+ border.b, 0 ),
-					z = parseNumberOrDefaultValue( this.css( 'z-index' ), 1 );
+            case REMOVE_COMMAND:
+				$(target).children('.' + SHEETIFY_CSS_CONTAINER_CLASS).remove();
+                break;
+            default:
+                // Remove any sheets from the current element
+                $(target).sheetify({command:REMOVE_COMMAND});
 
-				var sheet = createSheet( h, w, l, t, r, b, z );
-				var target = this;
+                var border = {
+                    t: parseNumberOrDefaultValue( target.css( 'border-top' ), 0 ),
+                    b: parseNumberOrDefaultValue( target.css( 'border-bottom' ), 0 ),
+                    l: parseNumberOrDefaultValue( target.css( 'border-left' ), 0 ),
+                    r: parseNumberOrDefaultValue( target.css( 'border-right' ), 0 )
+                };
+                var h = parseNumberOrDefaultValue( target.innerHeight(true), 0 ),
+                    w = parseNumberOrDefaultValue( target.innerWidth(false), 0 ),
+                    l = parseNumberOrDefaultValue( target.offset().left + border.l, 0 ),
+                    t = parseNumberOrDefaultValue( target.offset().top + border.t, 0 ),
+                    r = parseNumberOrDefaultValue( l+w+border.r, 0 ),
+                    b = parseNumberOrDefaultValue( t+h+ border.b, 0 ),
+                    z = parseNumberOrDefaultValue( target.css( 'z-index' ), 1 );
 
-				this.append(sheet);
+                var sheet = createSheet( h, w, l, t, r, b, z );
 
-				// Keeps the sheet's offset in sync with the target's offset.
-				initTargetOffsetMirroring(target, sheet);
+				target.append(sheet);
 
-				// Render prompt text if input provided.
-				if( options.message!==null&&options.message!==undefined ) {
-					var prompt = createPrompt();
-					sheet.append( prompt );
-					prompt.css( 'left', (this.innerWidth()-prompt.outerWidth(true))/2 );
-					prompt.css( 'top', (this.innerHeight()-prompt.outerHeight(true))/2 );
+                // Keeps the sheet's offset in sync with the target's offset.
+                initTargetOffsetMirroring(target, sheet);
+
+                // Render prompt text if input provided.
+                if( options.message!==null&&options.message!==undefined ) {
+                    var prompt = createPrompt();
+                    sheet.append( prompt );
+                    prompt.css( 'left', (target.innerWidth()-prompt.outerWidth(true))/2 );
+                    prompt.css( 'top', (target.innerHeight()-prompt.outerHeight(true))/2 );
+                }
+
+				// If options.hideAfterMillis is greater than zero, we're going to invoke 'remove' command in that
+				// amount of milliseconds after showing the sheet.
+				if (options.hideAfterMillis>0) {
+					setTimeout(function (){
+						$(target).sheetify({command:REMOVE_COMMAND});
+					}, options.hideAfterMillis);
 				}
-		}
+        }
 
-		function parseNumberOrDefaultValue(source, defaultValue,fn) {
-			fn = fn?fn:parseFloat;
-			return !isNaN(fn(source))?fn(source):defaultValue;
-		}
+        function parseNumberOrDefaultValue(source, defaultValue,fn) {
+            fn = fn?fn:parseFloat;
+            return !isNaN(fn(source))?fn(source):defaultValue;
+        }
 
-		function createSheet( h, w, l, t, r, b, z ) {
-			var sheet = $( '<div></div>' );
+        function createSheet( h, w, l, t, r, b, z ) {
+            var sheet = $( '<div></div>' );
 
-			sheet.addClass( SHEETIFY_CSS_CONTAINER_CLASS );
-			sheet.css( 'position', 'absolute' );
-			sheet.css( 'background-color', options.color );
-			sheet.css( 'opacity', options.opacity );
-			sheet.css( 'background-position', 'initial initial' );
-			sheet.css( 'background-repeat', 'initial initial' );
+            sheet.addClass( SHEETIFY_CSS_CONTAINER_CLASS );
+            sheet.css( 'position', 'absolute' );
+            sheet.css( 'background-color', options.color );
+            sheet.css( 'opacity', options.opacity );
+            sheet.css( 'background-position', 'initial initial' );
+            sheet.css( 'background-repeat', 'initial initial' );
 
-			if( h !== undefined ) {
-				sheet.css( 'height', h );
-			}
-			if( w !== undefined ) {
-				sheet.css( 'width', w );
-			}
-			if( l !== undefined ) {
-				sheet.css( 'left', l );
-			}
-			if( t !== undefined ) {
-				sheet.css( 'top', t );
-			}
-			if( r !== undefined ) {
-				sheet.css( 'right', r );
-			}
-			if( b !== undefined ) {
-				sheet.css( 'bottom', b );
-			}
-			if( z !== undefined ) {
-				sheet.css( 'z-index', z );
-			}
+            if( h !== undefined ) {
+                sheet.css( 'height', h );
+            }
+            if( w !== undefined ) {
+                sheet.css( 'width', w );
+            }
+            if( l !== undefined ) {
+                sheet.css( 'left', l );
+            }
+            if( t !== undefined ) {
+                sheet.css( 'top', t );
+            }
+            if( r !== undefined ) {
+                sheet.css( 'right', r );
+            }
+            if( b !== undefined ) {
+                sheet.css( 'bottom', b );
+            }
+            if( z !== undefined ) {
+                sheet.css( 'z-index', z );
+            }
 
-			if( options.altClassName ) {
-				sheet.addClass( options.altClassName );
-			}
+            if( options.altClassName ) {
+                sheet.addClass( options.altClassName );
+            }
 
-			return sheet;
-		}
+            return sheet;
+        }
 
         function initTargetOffsetMirroring($target, $sheet) {
             $target.resize(function() {
@@ -127,15 +137,15 @@
             $sheet.offset(target.offset());
         }
 
-		function createPrompt() {
-			var p =$( '<span></span>' ).html( options.message );
-			p.addClass( SHEETIFY_CSS_PROMPT_CLASS );
-			p.css( 'position', 'relative' );
-			p.css( 'display', 'inline-block' );
-			p.css( 'margin', 'auto auto' );
-			p.css( 'line-height', '1.2em' );
-			return p;
-		}
-	};
+        function createPrompt() {
+            var p =$( '<span></span>' ).html( options.message );
+            p.addClass( SHEETIFY_CSS_PROMPT_CLASS );
+            p.css( 'position', 'relative' );
+            p.css( 'display', 'inline-block' );
+            p.css( 'margin', 'auto auto' );
+            p.css( 'line-height', '1.2em' );
+            return p;
+        }
+    };
 
 })(window);
